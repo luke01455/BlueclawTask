e29b858d477cf9d95e17fc1116283b70d9b75ac5
luke01455 <luke0145555@hotmail.co.uk>
2020-02-24 13:46:01 +0000

    data displaying on screen, ready to be sorted by buttons

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './homepage.styles.scss';
 import { Table } from 'semantic-ui-react'
 import { data } from '../data/car-info'
 
 const HomePage = () => {
+    const [currentDisplay, setCurrentDisplay] = useState([0])
     console.log(data)
     return (
         <div className='homepage-container'>
-            <Table celled className="ui inverted table">
+            <Table celled className="ui table">
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell>Rank</Table.HeaderCell>
-                        <Table.HeaderCell>Manufacturer</Table.HeaderCell>
+                        <Table.HeaderCell>{data[currentDisplay].option}</Table.HeaderCell>
                         <Table.HeaderCell> Avg Emissions</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
+                    { data && 
+                    data[currentDisplay].rankings.map(carInfo => (
+                    <Table.Row key={carInfo.avgEmissions}>
+                        <Table.Cell className='selectable'> {carInfo.ranking}</Table.Cell>
+                        <Table.Cell >{carInfo.name}</Table.Cell>
+                        <Table.Cell>{carInfo.avgEmissions}</Table.Cell>
+                    </Table.Row>
+                    ))}
                   <Table.Row >
-                    <Table.Cell ></Table.Cell>
-                    <Table.Cell >2</Table.Cell>
-                    <Table.Cell>3</Table.Cell>
                   </Table.Row>
                 </Table.Body>
             </Table>
